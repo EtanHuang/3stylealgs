eaf721c5e2b4ef602bec119443e8eec1cd4f7562
EtanHuang <93726471+EtanHuang@users.noreply.github.com>
2025-08-19 22:45:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three'; // Import three.js
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+
 
 const piecesMap = new Map([
   ["UBL", 0], ["UBR", 1], ["UFL", 2], ["LBU", 3], ["LFU", 4], ["LDF", 5], ["LDB", 6],
